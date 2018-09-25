94555747d3dd47e82ab45e2a3ba5d3d9bbc72e7e
sbenstewart <sbenstewart@gmail.com>
2018-09-25 13:08:43 +0530

    Indentation fixes

--- 1/calctcp/Calc_Server.java
+++ 1/calctcp/Calc_Server.java
@@ -14,33 +14,33 @@ public class Calc_Server
         DataOutputStream dos = new DataOutputStream(s.getOutputStream());
         while (true)
         {
-            String input = dis.readUTF();
-            if(input.equals("bye"))
-                break;
-            System.out.println("Equation received:-" + input);
-            int result;
-            StringTokenizer st = new StringTokenizer(input);
-            int oprnd1 = Integer.parseInt(st.nextToken());
-            String operation = st.nextToken();
-            int oprnd2 = Integer.parseInt(st.nextToken());
-            if (operation.equals("+"))
-            {
-                result = oprnd1 + oprnd2;
-            }
-            else if (operation.equals("-"))
-            {
-                result = oprnd1 - oprnd2;
-            }
-            else if (operation.equals("*"))
-            {
-                result = oprnd1 * oprnd2;
-            }
-            else
-            {
-                result = oprnd1 / oprnd2;
-            }
-            System.out.println("Sending the result...");
-            dos.writeUTF(Integer.toString(result));
+          String input = dis.readUTF();
+          if(input.equals("bye"))
+              break;
+          System.out.println("Equation received:-" + input);
+          int result;
+          StringTokenizer st = new StringTokenizer(input);
+          int oprnd1 = Integer.parseInt(st.nextToken());
+          String operation = st.nextToken();
+          int oprnd2 = Integer.parseInt(st.nextToken());
+          if (operation.equals("+"))
+          {
+              result = oprnd1 + oprnd2;
+          }
+          else if (operation.equals("-"))
+          {
+              result = oprnd1 - oprnd2;
+          }
+          else if (operation.equals("*"))
+          {
+              result = oprnd1 * oprnd2;
+          }
+          else
+          {
+              result = oprnd1 / oprnd2;
+          }
+          System.out.println("Sending the result...");
+          dos.writeUTF(Integer.toString(result));
         }
     }
 }
