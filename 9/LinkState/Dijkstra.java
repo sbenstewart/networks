cdda7382527e3595172446907e5ad3b8ef4d2d26
sbenstewart <sbenstewart@gmail.com>
2018-10-03 14:26:24 +0530

    File fixes

--- 9/LinkState/Dijkstra.java
+++ 9/LinkState/Dijkstra.java
@@ -1,80 +1,61 @@
-/* Implementation of Link State Routing Protocol - Dijkstra's Algorithm */
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
-
 import javax.swing.JFileChooser;
-
-/* Dijkstra.java */
 public class Dijkstra {
-
 	static int Max_rooters;
-
-	static int DistGrph[][] = null;                    //Initialization and Declarations of variables
-	static int DistRow[] = null;
+	static int DistGrph[][] = null;
 	static int source = -1,destination = -1;
 	class ConTableEntry {
-		boolean flg; 
+		boolean flg;
 		int length;
 		int []ids;
 		int depth;
 	}
-
-	static ConTableEntry []conTable = null;            //Connection Table initialization
-
-	/* Main Method */	
+	static ConTableEntry []conTable = null;
 	public static void main(String[] args) {
-
-		Dijkstra psFrame = new Dijkstra();
-
+	Dijkstra psFrame = new Dijkstra();
 	}
-
 	public int nodeid(int in){
 		return (in+1);
 	}
-	/* ReadTextfileToBuildGraph Method  */
 	public void ReadTextfileToBuildGraph() {
 		try
 		{
 
-			System.out.println("Enter Text File name to read Network Topology Matrix:"); 
-			Scanner in1 = new Scanner(System.in);              //Takes from the user the input file name 
+			System.out.println("Enter Text File name to read Network Topology Matrix:");
+		Scanner in1 = new Scanner(System.in);
 			String fiename = in1.nextLine();
 
-			FileReader fr = new FileReader(fiename);           //Creation of FileReader object 
+			FileReader fr = new FileReader(fiename);
 			String val=new String();
-			BufferedReader br = new BufferedReader(fr);        //Creation of BufferedReader Object	
+			BufferedReader br = new BufferedReader(fr);
 			String[] temp;
 			val=br.readLine();
-
-			temp = val.split(" ");                            //to find size of the matrices or number of routers
+			temp = val.split(" ");
 			DistGrph = new int [temp.length][temp.length];
 			br.close();
 			fr.close();
 			System.out.println("<=======:Graph Size Read:=======>"+temp.length);
-
-			fr = new FileReader(fiename);                    //Read the content of file into the FileReader object    
+			fr = new FileReader(fiename);
 			val=new String();
-			br = new BufferedReader(fr);	
-			int i = 0;			
+			br = new BufferedReader(fr);
+			int i = 0;
 			while((val=br.readLine())!=null)
 			{
 				String[] temp1;
-				temp1 = val.split(" ");                  //Splitting with space as a Delimiter
+				temp1 = val.split(" ");
 				for (int dist =0; dist < temp1.length; dist++ ){
 					DistGrph[i][dist] =  Integer.parseInt(temp1[dist]);
 				}
 				i++;
 			}
 			br.close();
-			fr.close();			
+			fr.close();
 			System.out.println("<=======:Graph Table Initialized:=======>");
 			Max_rooters = i;
-
-
-			String imageName = "%3d" ;                      //Formatting to display the Content in the Matrix format
+			String imageName = "%3d" ;
 			System.out.println();
 			System.out.print("ID|");
 			for (int j =0; j < Max_rooters; j++ ){
@@ -83,7 +64,7 @@ public class Dijkstra {
 			System.out.println();
 			System.out.println("-------------------------------------------------------------------");
 			for (int j =0; j < Max_rooters; j++ ){
-				imageName = "%2d|" ;                   //Formatting to display the Content in the Matrix format
+				imageName = "%2d|" ;
 				System.out.print(String.format( imageName,nodeid(j)));
 				imageName = "%3d" ;
 				for (int k =0; k < Max_rooters; k++ )
@@ -92,62 +73,50 @@ public class Dijkstra {
 			}
 			System.out.println("-------------------------------------------------------------------");
 		} catch(Exception e){
-			System.out.println("File Not Found, Please Enter a Valid File !");    //To Handle File Not Found Exception
+			System.out.println("File Not Found, Please Enter a Valid File !");
 		}
 	}
-	/* ComputeConnectionTable Method*/	
 	public void ComputeConnectionTabel(){
-
 		if (DistGrph[source][source] == 0) {
-			conTable = new ConTableEntry[Max_rooters];    //Creates a ConTable Object with the Number of Routers
+			conTable = new ConTableEntry[Max_rooters];
 
-			for (int j = 0;j<Max_rooters;j++) {	
+			for (int j = 0;j<Max_rooters;j++) {
 				ConTableEntry ce = new ConTableEntry();
 				ce.flg = true;
-				ce.length = -1;                      //Initializing the variables to start processing            
+				ce.length = -1;
 				ce.ids = new int[Max_rooters];
 				ce.ids[0] = source ;
 				ce.depth = 1;
 				for (int i = 1;i<Max_rooters;i++) ce.ids[i] = -1;
 				conTable[j] = ce;
 			}
-
-			//initializing source in working row 
 			int tmpsorce = source;
 			conTable[tmpsorce].length = 0;
 			conTable[tmpsorce].ids[0]=source;
 			conTable[tmpsorce].flg = false;
-			//int nodedepth = 1;
-
 			for (int loopcnt = 0 ; loopcnt<Max_rooters; loopcnt++) {
-
 				for (int k = 0 ;  k< Max_rooters ; k++)
-				{  
+				{
 					if (conTable[k].flg)
 					{
 						if (DistGrph[tmpsorce][k]!= -1){
-
 							if ((conTable[k].length != -1) ) {
-								// smaller ( selected node length+ tableentry,previous entry path) 
 								if (conTable[k].length > conTable[tmpsorce].length + DistGrph[tmpsorce][k]) {
 									conTable[k].length = conTable[tmpsorce].length + DistGrph[tmpsorce][k];
 									for (int idx = 0; idx< conTable[tmpsorce].depth ;idx ++)
 										conTable[k].ids[idx] = conTable[tmpsorce].ids[idx];
-									conTable[k].depth = conTable[tmpsorce].depth ;								
+									conTable[k].depth = conTable[tmpsorce].depth ;
 									conTable[k].ids[conTable[k].depth] = k;
 									conTable[k].depth++;
 								}
 							}
-							else 
-							{  //selected node length is added to length table entry for new length
+							else
+							{
 								conTable[k].length = conTable[tmpsorce].length + DistGrph[tmpsorce][k];
-
 								for (int idx = 0; idx< conTable[tmpsorce].depth ;idx ++){
 									conTable[k].ids[idx] = conTable[tmpsorce].ids[idx];
 								}
-
-
-								conTable[k].depth = conTable[tmpsorce].depth ;	
+								conTable[k].depth = conTable[tmpsorce].depth ;
 								conTable[k].ids[conTable[k].depth] = k;
 								conTable[k].depth++;
 							}
@@ -155,48 +124,42 @@ public class Dijkstra {
 
 					}
 				}
-
 				for (int i = 0; i<Max_rooters; i++){
 					//System.out.print(" "+conTable[i].length);
 				}
 				System.out.println();
-				//initalize smallest dist
 				int small = 0;
 				int indx_small = 0;
-
 				for (int i = 0; i<Max_rooters; i++){
-
 					if (conTable[i].flg){
 						if(conTable[i].length !=-1 ){
 							small = conTable[i].length;
 							indx_small = i;
 							break;
 						}
-					}				
+					}
 				}
-				//find source for next iteration
 				for (int i = 0; i<Max_rooters; i++){
 					if (conTable[i].flg){
 						if(conTable[i].length != -1 ){
-							if (small > conTable[i].length){						
+							if (small > conTable[i].length){
 								small = conTable[i].length;
 								indx_small = i;
 							}
 						}
-					}			
+					}
 				}
 				tmpsorce = indx_small;
 				conTable[tmpsorce].flg = false;
-
-			}	
+			}
 
 			System.out.println("Router [" + nodeid(source) + "] "+ "Connection Table:");
 			System.out.println("============================");
-			System.out.println("Destination        Interface");	         //Printing the Connection Table
+			System.out.println("Destination        Interface");
 			for (int i = 0; i<Max_rooters; i++){
 				String tmp = String.valueOf(conTable[i].ids[1]+1);
-				if (conTable[i].ids[1] == -1) tmp = "-1";                //Check the Router ID if it is -1
-				if (i == source) tmp = "-";                              //Source to Source Router will be "-"
+				if (conTable[i].ids[1] == -1) tmp = "-1";
+				if (i == source) tmp = "-";
 				System.out.print("      "+  nodeid(i) + "                "+ tmp);
 				System.out.println();
 			}
@@ -204,28 +167,25 @@ public class Dijkstra {
 		else {
 			System.out.println("Router [" + nodeid(source) + "] "+ "Connection Table:");
 			System.out.println("============================");
-			System.out.println("Destination        Interface");              //If there is no Interface to the router then assign -1 
+			System.out.println("Destination        Interface");
 			for (int i = 0; i<Max_rooters; i++){
 				System.out.print("      "+  nodeid(i) + "                -1");
 				System.out.println();
 			}
-
 		}
 	}
-	/* PrintConnectionTable Method */	
-	public void PrintConnectionTabel() {	
-		System.out.println("Enter Source Rooter Id< 1 - "+ (Max_rooters)+" >:"); 
-		Scanner in1 = new Scanner(System.in);       //Takes input from the User for the Source Router ID                      
+	public void PrintConnectionTabel() {
+		System.out.println("Enter Source Rooter Id< 1 - "+ (Max_rooters)+" >:");
+		Scanner in1 = new Scanner(System.in);
 		String str_source = in1.nextLine();
 		source = Integer.parseInt(str_source);
-		source--;                                   //Decrements the Source ID by 1
-		ComputeConnectionTabel();                   //Invoke ComputeConnectionTable Method
+		source--;
+		ComputeConnectionTabel();
 	}
-	/* PrintShortPathToDestination Method */
 	public void PrintShortPathToDestination() {
 
-		System.out.println("Enter Destination Rooter Id< 1 - "+ (Max_rooters)+" >:"); 
-		Scanner in1 = new Scanner(System.in);       //Takes from the user the Destination Router ID as input 
+		System.out.println("Enter Destination Rooter Id< 1 - "+ (Max_rooters)+" >:");
+		Scanner in1 = new Scanner(System.in);       //Takes from the user the Destination Router ID as input
 		String str_dest = in1.nextLine();
 		destination = Integer.parseInt(str_dest);
 		destination--;                              //Decrements Destination Router ID
@@ -248,8 +208,8 @@ public class Dijkstra {
 	/* ChangeNetworkTopology */
 	public void ChangeNetworkTopology(){
 
-		System.out.println("Enter Rooter Id< 1 - "+ (Max_rooters)+" > to Down:");                       
-		Scanner in1 = new Scanner(System.in);          //Takes from the user the Router ID to Down as input 
+		System.out.println("Enter Rooter Id< 1 - "+ (Max_rooters)+" > to Down:");
+		Scanner in1 = new Scanner(System.in);          //Takes from the user the Router ID to Down as input
 		String str_delt = in1.nextLine();
 		int delid = Integer.parseInt(str_delt);
 		delid--;
@@ -283,32 +243,32 @@ public class Dijkstra {
 		System.out.println("-------------------------------------------------------------------");
 
 	}
-	/* MENU */	
+	/* MENU */
 	public Dijkstra() {
 
 		while (true){
 			System.out.println("===========================================================\n");
 			System.out.println("Dijkstra's Algorithm - Link State Routing Simulator:");
 			System.out.println("===========================================================\n");
-			System.out.println("Enter The Option :\n==================\n1. Create a Network Topology\n \n2. Build a Connection Table \n \n3. Shortest Path to Destination Router \n \n4. Modify a topology \n \n5. Exit\n");	
+			System.out.println("Enter The Option :\n==================\n1. Create a Network Topology\n \n2. Build a Connection Table \n \n3. Shortest Path to Destination Router \n \n4. Modify a topology \n \n5. Exit\n");
 			System.out.println("Command:");
 			Scanner in = new Scanner(System.in);
 			String regmessage = in.nextLine();
 
-			if (regmessage.equals("1")){				
-				ReadTextfileToBuildGraph();             //ReadTextFiletoBuildGraph method call                       
+			if (regmessage.equals("1")){
+				ReadTextfileToBuildGraph();             //ReadTextFiletoBuildGraph method call
 				for (int n = 0;n<Max_rooters;n++ ) {    //EXTRA FEATURE IMPLEMENTATION -> TO DISPLAY CONNECTION TABLE FOR ALL NODES
 					source = n;
 					ComputeConnectionTabel();       //ComputeConnectionTable method call
 					System.out.println();
 				}
-			}				
+			}
 			if (regmessage.equals("2")){
-				PrintConnectionTabel();                 //PrintConnectionTable method call     
-			}		
+				PrintConnectionTabel();                 //PrintConnectionTable method call
+			}
 			if (regmessage.equals("3")){
-				PrintShortPathToDestination();          //PrintShortPathToDestination method call        
-			}		
+				PrintShortPathToDestination();          //PrintShortPathToDestination method call
+			}
 			if (regmessage.equals("4")){
 				ChangeNetworkTopology();                //ChangeNetworkTopology method call
 				if ((source >-1) && (source < Max_rooters)){
@@ -332,10 +292,10 @@ public class Dijkstra {
 
 				}else System.out.println("Source node is not selected");
 			}
-			if (regmessage.equals("5")){				
+			if (regmessage.equals("5")){
 				System.out.println("Exit LinkStateRouting project. Good Bye!.");
-				System.exit(0);   	//Exit system call	
-			}	
+				System.exit(0);   	//Exit system call
+			}
 		}
 	}
 }
